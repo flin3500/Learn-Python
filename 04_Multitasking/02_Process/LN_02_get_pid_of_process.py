555ced81a68dc653ba8c3878c734272db082b206
Lin <lin.acandc@gmail.com>
2020-07-29 23:27:52 +1000

    add process

---
+++ 04_Multitasking/02_Process/LN_02_get_pid_of_process.py
@@ -0,0 +1,19 @@
+import multiprocessing
+import os
+import time
+
+
+def test():
+    while True:
+        print("----in child process pid=%d , parent process pid=%d---" % (os.getpid(), os.getppid()))
+        time.sleep(1)
+
+
+def main():
+    print("----in main process pid=%d---parent process pid=%d----" % (os.getpid(), os.getppid()))
+    p = multiprocessing.Process(target=test)
+    p.start()
+
+
+if __name__ == "__main__":
+    main()

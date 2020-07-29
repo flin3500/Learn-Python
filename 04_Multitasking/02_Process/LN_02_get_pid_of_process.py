fa5daec6dac1258c7a810dcd94b448c6f311d404
Lin <lin.acandc@gmail.com>
2020-07-29 23:54:28 +1000

    add process

--- 04_Multitasking/02_Process/LN_02_get_pid_of_process.py
+++ 04_Multitasking/02_Process/LN_02_get_pid_of_process.py
@@ -6,14 +6,12 @@ import time
 def test():
     while True:
         print("----in child process pid=%d , parent process pid=%d---" % (os.getpid(), os.getppid()))
-        time.sleep(1)
+        time.sleep(100)
 
 
 def main():
     print("----in main process pid=%d---parent process pid=%d----" % (os.getpid(), os.getppid()))
     p = multiprocessing.Process(target=test)
-    while True:
-        continue
     p.start()
 
 
